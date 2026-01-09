756b2ba38c8454ebf7f9240b3df19f38259016b7
Ayyan0417 <mohammedayyan03@gmail.com>
2026-01-09 11:05:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,26 @@ let data = JSON.parse(localStorage.getItem("healthData")) || [];
 
 function showData() {
   records.innerHTML = "";
-  data.forEach(item => {
+
+  data.forEach((item, index) => {
     records.innerHTML += `
       <tr>
         <td>${item.date}</td>
         <td>${item.weight || "-"}</td>
         <td>${item.steps || "-"}</td>
         <td>${item.water || "-"}</td>
+        <td>
+          <button onclick="deleteRecord(${index})">❌ Delete</button>
+        </td>
       </tr>
     `;
   });
+}
+
+function deleteRecord(index) {
+  data.splice(index, 1);
+  localStorage.setItem("healthData", JSON.stringify(data));
+  showData();
 }
 
 form.addEventListener("submit", function (e) {
